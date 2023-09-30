8b4e97a0a7582410599a8700a30aaa05ef4f0588
Mohit <pundemohit@gmail.com>
2023-10-01 00:00:03 +0530

    MERN

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,20 +5,20 @@ import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div>
-        <div className="">
-            <Navbar/>
-            <div>
-              <div className="card" style="width: 18rem;">
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  
-                </div>
-              </div>
+      <div className="">
+        <Navbar />
+        <div>
+          <div className="card mt-3" style={{ "width": "18rem", "maxHeight":"360px" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
             </div>
-            <Footer></Footer>
+          </div>
         </div>
+        <Footer></Footer>
+      </div>
     </div>
   )
 }
